8b56c935992bb10c93ad43a24d2cc999559bc29b
thejennywang <thejennywang@gmail.com>
2014-07-15 21:12:28 +0100

    Cleaned up fixes

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -29,17 +29,9 @@ describe Airport do
 
     it 'a plane cannot land if the airport is full' do
       fill_in(airport)
-      # expect(self).to receive(:puts).with("The aiport is full. Keep on flying.")
      	expect(airport.land(plane)).to eq "The aiport is full. Keep on flying."
     end
     
-    # Include a weather condition using a module.
-    # The weather must be random and only have two states "sunny" or "stormy".
-    # Try and take off a plane, but if the weather is stormy, the plane can not take off and must remain in the airport.
-    # 
-    # This will require stubbing to stop the random return of the weather.
-    # If the airport has a weather condition of stormy,
-    # the plane can not land, and must not be in the airport
     context 'weather conditions' do
 
       before(:each) do
@@ -65,11 +57,6 @@ def fill_in(airport)
     end
   end
 
-# grand final
-# Given 6 planes, each plane must land. When the airport is full, every plane must take off again.
-# Be careful of the weather, it could be stormy!
-# Check when all the planes have landed that they have the right status "landed"
-# Once all the planes are in the air again, check that they have the status of flying!
 describe "The grand finale (last spec)" do
   it 'all planes can land' do
     allow(STDOUT).to receive(:puts)
