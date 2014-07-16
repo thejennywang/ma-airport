3829f6f5a2fb595f669e4ffa9ed27eea268c9293
thejennywang <thejennywang@gmail.com>
2014-07-16 13:06:19 +0100

    fixed bugs

--- lib/plane.rb
+++ lib/plane.rb
@@ -9,13 +9,17 @@ class Plane
 	end
 
 	def take_off_from(airport)
-		return "You cannot take off when already flying. :)" if @status = :flying
+		return "You cannot take off when already flying." if @status == :flying
 		@status = :flying
+		airport.take_off(self)
+		self
 	end
 
 	def land_on(airport)
-		return "You cannot land when you're already landed. :)" if @status = :landed
+		return "You cannot land when you're already landed." if @status == :landed
 		@status = :landed
+		airport.land(self)
+		self
 	end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -15,16 +15,20 @@ describe Plane do
   end
 
   it 'can land' do
+    allow(airport).to receive(:land)
     plane.land_on(airport)
     expect(plane.status).to eq :landed
   end
 
   it 'cannot take off when already flying' do
-    expect(plane.take_off_from(airport)).to eq "You cannot take off when already flying. :)"
+    plane.take_off_from(airport)
+    expect(plane.take_off_from(airport)).to eq "You cannot take off when already flying."
   end
 
   it 'cannot land when already landed' do
-    expect(plane.land_on(airport)).to eq "You cannot land when you're already landed. :)"
+    allow(airport).to receive(:land)
+    plane.land_on(airport)
+    expect(plane.land_on(airport)).to eq "You cannot land when you're already landed."
   end
 
 end
